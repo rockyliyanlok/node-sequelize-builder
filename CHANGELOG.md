f689605becbffe5c8a061508a316185d53acb1d9
Rocky Li <rocky.li@sandewisdom.com>
2019-04-30 14:18:09 +0800

    updated limit maximum to 1000

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+1.0.4 / Apr 30, 2019
+==================
+  * updated limit maximum value to 1000
+
 1.0.1 / Mar 28, 2019
 ==================
   * updated package name
--- src/parser/parse-offset-limit.js
+++ src/parser/parse-offset-limit.js
@@ -2,12 +2,20 @@
 
 const _ = require('lodash')
 
+const maxLimit = 1000
 const parseOffsetLimit = (Model, req = {}) => {
   req.query = req.query || {}
   const { offset, limit } = req.query
   const options = {}
-  options.offset = !_.isNil(offset) ? parseInt(offset) : 0
-  options.limit = !_.isNil(limit) ? parseInt(limit) : 50
+  if (_.isNil(limit)) {
+    options.limit = maxLimit
+    options.offset = _.isNil(offset) ? 0 : parseInt(offset)
+  } else {
+    if (parseInt(limit) >= 0) {
+      options.limit = parseInt(limit) > maxLimit ? maxLimit : parseInt(limit)
+      options.offset = _.isNil(offset) ? 0 : parseInt(offset)
+    }
+  }
   return options
 }
 
